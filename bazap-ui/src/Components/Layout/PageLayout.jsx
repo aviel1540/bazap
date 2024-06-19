a4bd0f7192c03ae4bcb22c18f6aaa067e4994854
Nir Kusan <nir.kusan@novideasoft.com>
2024-06-19 14:47:08 +0300

    change the icon, start to work on classified devicetype

--- bazap-ui/src/Components/Layout/PageLayout.jsx
+++ bazap-ui/src/Components/Layout/PageLayout.jsx
@@ -2,20 +2,24 @@ import { Layout } from "antd";
 import PropTypes from "prop-types";
 import Breadcrumbs from "./Breadcrumbs";
 import MainNavigation from "./Navbar/MainNavigation";
+import { Footer } from "antd/es/layout/layout";
 const { Content } = Layout;
 const PageLayout = ({ children }) => {
     return (
-        <Layout>
+        <Layout style={{minHeight:"100vh"}}>
             <MainNavigation />
             <Content
                 style={{
                     padding: "0px 76px",
-                    margin: "15px 0px",
+                    margin: "15px 15px",
                 }}
             >
                 <Breadcrumbs />
                 {children}
             </Content>
+            <Footer style={{ textAlign: "center", position: "sticky", bottom: "0" }}>
+                Ant Design ©{new Date().getFullYear()} Created by Ant UED
+            </Footer>
         </Layout>
     );
 };
